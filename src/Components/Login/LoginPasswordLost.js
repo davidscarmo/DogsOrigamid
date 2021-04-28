5568e5bbe0e8722663ab24b121aa2f520d694bb4
David Carmo <davidsc2812@gmail.com>
2021-04-28 10:02:58 -0300

    Estruturacao inicial do login

---
+++ src/Components/Login/LoginPasswordLost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoginPasswordLost() {
+  return (
+    <div>
+      Perdeu Senha
+    </div>
+  )
+}
+
+export default LoginPasswordLost
---
+++ src/Components/Login/LoginPasswordReset.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoginPasswordReset() {
+  return <div>Resetar Senha</div>;
+}
+
+export default LoginPasswordReset;
