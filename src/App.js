f4ec4c15d7b51b4e7fa8f7510df424940772f747
David Carmo <davidsc2812@gmail.com>
2021-04-28 09:29:48 -0300

    Estruturacao do componente Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
-import Api from "./api/Api";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css'; 
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+import Login from "./Components/Login/Login";
 
-import "./App.css";
 function App() {
-  return <div><Api /></div>;
+  return <div>
+    <BrowserRouter >
+    <Header /> 
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Login />}/>
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
