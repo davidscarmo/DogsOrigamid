948d464c5ead7911abf40fde1033140cda030828
David Carmo <davidsc2812@gmail.com>
2021-05-02 23:17:45 -0300

    Estruturacao inicial do userPhoto

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -1,7 +1,32 @@
 import React from "react";
+import styles from "./UserPhotoPost.module.css";
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
 
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
 const UserPhotoPost = () => {
-  return <div>Postar Foto</div>;
+  const nome = useForm();
+  const peso = useForm('number');
+  const idade = useForm('number');
+  const [img, setImg] = React.useState({});
+  const {data, error, loading, request} = useFetch();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleImgChange = () => {
+    
+  }
+  return <section className={`${styles.photoPhost} animeLeft`}>
+    <form onSubmit={handleSubmit}>
+    <Input label="Nome" type="text" name="nome"/>
+    <Input label="Peso" type="text" name="peso"/>
+    <Input label="Idade" type="text" name="idade"/>
+    <input type="file" name="img" id="img" onChange={handleImgChange} />
+    <Button>Enviar</Button>
+    </form>
+  </section>
 };
 
 export default UserPhotoPost;
