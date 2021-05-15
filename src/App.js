faf0d03ff52d89720b6116d561121628ca5b1cef
David Carmo <davidsc2812@gmail.com>
2021-05-14 22:39:09 -0300

    Criacao do componente  de visualizacao do usuario como visualizador

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {UserStorage} from './UserContext';
 import User from "./Components/User/User";
 import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
 function App() {
   return <div>
     <BrowserRouter >
@@ -18,6 +19,7 @@ function App() {
       <Route path="/" element={<Home />}/>
       <Route path="login/*" element={<Login />}/>
       <Route path="foto/:id" element={<Photo />}/>
+      <Route path="perfil/:user" element={<UserProfile />}/>
       <ProtectedRoute path="conta/*" element={<User />}/>
     </Routes>
     <Footer />
