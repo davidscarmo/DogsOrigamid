6a6ed41943f1095043758c0ee8c4939ea5377141
David Carmo <davidsc2812@gmail.com>
2021-05-16 04:39:58 -0300

    Adicao da biblioteca de gráficos Victory ao projeto e utilizacao da mesma, estilzacao do component de estatisticas

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -15,7 +15,7 @@ const FeedPhotos = ({ user, page, setModalPhoto, setInfinite }) => {
       const { url, options } = PHOTOS_GET({ page, total, user });
       const { response, json } = await request(url, options);
       console.log('Request: ' + json);
-      if (response && response.ok && json.length < 3) setInfinite(false);
+      if (response && response.ok && json.length < 6) setInfinite(false);
     };
     fetchPhotos();
   }, [request, user, page, setInfinite]);
--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { STATS_GET } from "../../Api";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
 import Head from "../Helper/Head";
+import Loading from "../Helper/Loading";
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"));
+
 
 const UserStats = () => {
-  return <div> 
-    <Head title="Estatísticas" description="Página com as estatísticas do usuário" />
-    User estatisticas</div>;
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    };
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data) return (
+      <React.Suspense fallback={<></>}>
+        <Head
+          title="Estatísticas"
+          description="Página com as estatísticas do usuário"
+        />
+        <UserStatsGraphs  data={data}/>
+      </React.Suspense>
+    );
+  else return null;
 };
 
 export default UserStats;
